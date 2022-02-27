fc4c1dbfef8b2c7d42280697cd92c416fa4cdd41
fatlhr <fatiharahmat257@gmail.com>
2022-02-27 12:04:50 +0700

    detail page responsive

--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_trip_app_cubit_api/misc/colors.dart';
+import 'package:flutter_trip_app_cubit_api/widgets/app_text.dart';
 
 class ResponsiveButton extends StatelessWidget {
-  bool? isResponsive;
-  double? width;
+  final bool? isResponsive;
+  final double? width;
 
-  ResponsiveButton({Key? key, this.isResponsive = false, this.width})
+  const ResponsiveButton({Key? key, this.isResponsive = false, this.width = 100})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: AppColors.mainColor,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset("img/button-one.png"),
-        ],
+    return Flexible(
+      child: Container(
+        width:isResponsive == true?double.maxFinite: width,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.mainColor,
+        ),
+        child: Row(
+          mainAxisAlignment: isResponsive == true
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.center,
+          children: [
+            isResponsive == true
+                ? Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: AppText(
+                      text: 'Book Trip Now',
+                      color: Colors.white,
+                    ),
+                  )
+                : Container(),
+            Image.asset("img/button-one.png"),
+          ],
+        ),
       ),
     );
   }
